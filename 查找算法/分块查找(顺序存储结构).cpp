e1713049cd7997260bfc500f7e74142192364071
Wecccccccc <80095598+Wecccccccc@users.noreply.github.com>
2021-05-08 22:52:00 +0800

    Update 分块查找(顺序存储结构).cpp

--- 查找算法/分块查找(顺序存储结构).cpp
+++ 查找算法/分块查找(顺序存储结构).cpp
@@ -1,7 +1,7 @@
 #include <iostream>
 using namespace std;
 const int Maxsize = 1000;
-const int MINNUM = -1e8;
+const int MINNUM = -999999;
 class Index_table
 {
 	friend class SeqList;
@@ -12,7 +12,7 @@ private:
 
 class SeqList
 {
-//该顺序表从下标为0开始
+	//该顺序表从下标为0开始
 public:
 	~SeqList()
 	{
@@ -20,6 +20,8 @@ public:
 		delete[] idxtab;
 	};
 	void CreateList();
+	void Print_Elem_k();
+	void Print_Elem_w();
 	int  Find_Elem(int k);//查找值为k的序号
 private:
 	Index_table *idxtab;
@@ -30,8 +32,10 @@ private:
 	}
 	int *elem;
 	int len;
+	int len_w;
 	int N;
 };
+
 
 void SeqList::CreateList()
 {
@@ -39,31 +43,71 @@ void SeqList::CreateList()
 	int n;
 	cin >> n;
 	elem = new int[n];
+	len_w = n;
 	cout << "请依次输入顺序表中的数" << endl;
 	for (int i = 0; i < n; i++) cin >> elem[i];
 	cout << "请输入每大份中有多少小份" << endl;
 	cin >> N;
-	int m = (n/N)+1;
+	int m = (n / N) + 1;
 	idxtab = new Index_table[m];
+	len = m;
 	for (int i = 0; i < m; i++) idxtab[i].address = i * N;
 	for (int i = 0; i < m; i++)
 	{
 		int maxv = MINNUM;
 		for (int j = 0 + i * N; j < N*(i + 1) && j < n; j++)
-		{
 			maxv = max(maxv, elem[j]);
-		}
 		idxtab[i].key = maxv;
 	}
+	for (int i = 0; i < len - 1; i++)//选择排序给查找表排序
+	{
+		int minv = i;
+		for (int j = i + 1; j < len; j++)
+			if (idxtab[j].key < idxtab[minv].key) minv = j;
+		if (i != minv)
+		{
+			int tmp1 = idxtab[i].key;
+			int tmp2 = idxtab[i].address;
+			idxtab[i].key = idxtab[minv].key;
+			idxtab[i].address = idxtab[minv].address;
+			idxtab[minv].key = tmp1;
+			idxtab[minv].address = tmp2;
+		}
+	}
 }
+
+
+void SeqList::Print_Elem_k()
+{
+	for (int i = 0; i < len; i++) cout << idxtab[i].key << " ";
+	cout << endl;
+	for (int i = 0; i < len; i++) cout << idxtab[i].address << " ";
+	cout << endl;
+}
+
+void SeqList::Print_Elem_w()
+{
+	for (int i = 0; i < len_w; i++) cout << elem[i] << " ";
+	cout << endl;
+}
+
 
 int SeqList::Find_Elem(int k)
 {
 	int j = 0;
-	while (idxtab[j].key < k) j++;
-	int f = idxtab[j].address;
+	if (k > idxtab[len - 1].key) return -1;
+	if (k < idxtab[0].key)//如果k小于查找表的最小值，则要遍历全部。
+	{
+		for (int i = 0; i < len_w; i++)
+			if (elem[i] == k) return i;
+	}
+	else
+	{
+		while (idxtab[j].key < k) j++;
+		int f = idxtab[j].address;
 		for (int i = idxtab[j].address; i < f + N; i++)
 			if (elem[i] == k) return i;
+	}
 	return -1;
 }
 
@@ -73,6 +117,11 @@ int main()
 	l.CreateList();
 	int k;
 	cin >> k;
-	cout<<l.Find_Elem(k)<<endl;
+	cout << l.Find_Elem(k) << endl;
+	l.Print_Elem_w();
+	l.Print_Elem_k();
 	return 0;
 }
+
+
+
